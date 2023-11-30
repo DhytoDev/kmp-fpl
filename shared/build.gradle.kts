ed13001e4a5728b23e68d89aa23a9bd43d754fe6
Fordyta Abubakar <dhytodev@gmail.com>
2023-11-30 17:11:51 +0800

    chore: add logging dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -59,6 +59,8 @@ kotlin {
                 implementation(libs.ktor.client.core)
                 implementation(libs.ktor.client.content.negotiation)
                 implementation(libs.ktor.serialization.kotlinx.json)
+                implementation(libs.ktor.client.logging)
+                implementation(libs.logging.kermit)
 
                 implementation(compose.runtime)
                 implementation(compose.foundation)
