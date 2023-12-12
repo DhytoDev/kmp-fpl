daf2d10ce13f4baecc0f2c9d99f43501e5258fba
Fordyta Abubakar <dhytodev@gmail.com>
2023-12-13 00:16:48 +0800

    feat: add LocalFailure

--- shared/src/commonMain/kotlin/dev/dhyto/fpl/shared/domain/base/Failure.kt
+++ shared/src/commonMain/kotlin/dev/dhyto/fpl/shared/domain/base/Failure.kt
@@ -2,4 +2,5 @@ package dev.dhyto.fpl.shared.domain.base
 
 sealed class Failure(open val message: String?) {
     data class NetworkFailure(override val message: String?) : Failure(message)
+    data class LocalFailure(override val message: String?) : Failure(message)
 }
